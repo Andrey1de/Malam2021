2a3a34868db8066c2444c055a6bf9e02f6950737
Andrey1de <andrey1de@gmail.com>
2021-12-28 05:01:28 +0200

    22

--- Malam2021/Controllers/WebRootPathController.cs
+++ Malam2021/Controllers/WebRootPathController.cs
@@ -3,6 +3,7 @@ using Malam2021.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,42 +12,43 @@ namespace Malam2021.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class WebRootPathCController : ControllerBase
+    public class WebRootPathController : ControllerBase
     {
         private readonly IWebHostEnvironment env;
 
-        public WebRootPathCController(IWebHostEnvironment _env)
+        public WebRootPathController(IWebHostEnvironment _env)
         {
             env = _env;
         }
-        // GET: api/<WebRootPathCController>
+        // GET: api/<WebRootPathController>
+        
         [HttpGet]
-        public string Get()
+        public string Get(string file)
         {
-            var ret = env.WebRootPath;
-            return ret;
+            var path = env.WebRootPath;
+            return Path.Combine(path, file);
         }
 
-        //// GET api/<WebRootPathCController>/5
+        //// GET api/<WebRootPathController>/5
         //[HttpGet("{id}")]
         //public Department Get(int id)
         //{
         //    return dataAccessService.GetDepartment(id);
         //}
 
-        //// POST api/<WebRootPathCController>
+        //// POST api/<WebRootPathController>
         //[HttpPost]
         //public void Post([FromBody] string value)
         //{
         //}
 
-        //// PUT api/<WebRootPathCController>/5
+        //// PUT api/<WebRootPathController>/5
         //[HttpPut("{id}")]
         //public void Put(int id, [FromBody] string value)
         //{
         //}
 
-        //// DELETE api/<WebRootPathCController>/5
+        //// DELETE api/<WebRootPathController>/5
         //[HttpDelete("{id}")]
         //public void Delete(int id)
         //{
